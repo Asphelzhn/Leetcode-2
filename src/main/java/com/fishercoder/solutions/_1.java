6f5d255c1944349091f19988bd5cde8e18b51e49
asphelzhn <568718851@qq.com>
2019-03-06 13:30:28 +0800

    change01

--- src/main/java/com/fishercoder/solutions/_1.java
+++ src/main/java/com/fishercoder/solutions/_1.java
@@ -14,6 +14,22 @@ import java.util.Map;
  * return [0, 1].
  */
 public class _1 {
+	
+	/*public int[] twoSum(int[] nums, int target) {
+		int [] indices = new int [2];
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if(( nums[i]+nums[j] ) == target)
+				{
+					indices [0] = i;
+					indices [1] = j;
+					
+				}
+			}
+		}
+		return indices;
+
+	}*/
 
     public static class Solution1 {
         public int[] twoSum(int[] nums, int target) {
